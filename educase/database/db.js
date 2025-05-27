649ac8913c9db3207de639bf2aba00624df2616e
Advitiya Prakash <147964490+SilentCanary@users.noreply.github.com>
2025-05-27 12:49:45 +0530

    Update db.js

--- educase/database/db.js
+++ educase/database/db.js
@@ -1,10 +1,10 @@
 const mysql=require('mysql2');
-
+require('dotenv').config();
 const connection=mysql.createConnection({
-    host:'sql12.freesqldatabase.com',
-    user:'sql12781456',
-    password:'UsXRgwjwTh',
-    database:'sql12781456'
+    host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 connection.connect(err=>{
